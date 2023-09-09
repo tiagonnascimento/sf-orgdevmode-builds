0474cb76b7615f088d5207f7e9e2088bf9439058
Jaime Terrats <jterrats@salesforce.com>
2023-09-08 18:10:31 -0700

    feat(profiler): create generate command to fetch profiles from targeted org

---
+++ test/commands/generate/profiler.test.ts
@@ -0,0 +1,48 @@
+/* eslint-disable */
+import { MockTestOrgData, TestContext } from '@salesforce/core/lib/testSetup';
+import { expect } from 'chai';
+import { stubSfCommandUx } from '@salesforce/sf-plugins-core';
+import { TestSession } from '@salesforce/cli-plugins-testkit';
+import GenerateProfiler from '../../../src/commands/generate/profiler';
+
+describe('generate profiler', () => {
+  const $$ = new TestContext();
+  let sfCommandUxStubs: ReturnType<typeof stubSfCommandUx>;
+  let testOrg: MockTestOrgData;
+  let session: TestSession;
+  let sourcePath: string;
+
+  beforeEach(async () => {
+    testOrg = new MockTestOrgData();
+    testOrg.orgId = '00Dxx0000000000';
+    testOrg.aliases = ['testdemo'];
+    sfCommandUxStubs = stubSfCommandUx($$.SANDBOX);
+    session = await TestSession.create({
+      project: {
+        gitClone: 'https://github.com/trailheadapps/dreamhouse-lwc.git',
+      },
+      devhubAuthStrategy: 'AUTO',
+    });
+    sourcePath = session.dir + '/dreamhouse-lwc/force-app';
+  });
+
+  afterEach(async () => {
+    $$.restore();
+    session.clean();
+  });
+
+  it('fetch profile data without passing any flag to the command', async () => {
+    $$.stubAliases({ nonscratchalias: testOrg.username });
+    await $$.stubAuths(testOrg);
+    console.log(
+      await GenerateProfiler.run(['--username', testOrg.username, '--project-only', 'true', '--path', sourcePath])
+    );
+    const data = sfCommandUxStubs.log
+      .getCalls()
+      .flatMap((c) => {
+        c.args;
+      })
+      .join('\n');
+    expect(data).to.include('success');
+  });
+});
